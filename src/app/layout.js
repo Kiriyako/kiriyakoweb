946e443bd9d89b49778bb44e844baa926a6d6b4b
Kiriyako <doraemonboy158@gmail.com>
2023-09-23 15:47:05 +0530

    stuff

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Poppins } from 'next/font/google'
 import Navbar from './components/navbar/Nav'
 const poppins = Poppins({ subsets: ['latin'], weight: ['400']})
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Kiriyako',
@@ -11,7 +12,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        <meta name="google-site-verification" content="JdOLYNfU2F0Kn8meIhccpZ-757-A9fAco-6MO5loKl0"/>
+      </head>
       <body className={poppins.className}>
+        <Script id="google-analytics" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-T4CM2RZNV4" />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T4CM2RZNV4');
+          `}
+        </Script>
         <Navbar />
         {children}
         </body>
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,10 @@ export default function Main(){
    <div id="main">
   
    <h1>Hi, I am Kiriyako.</h1>
-
+<div id="vid">
+<iframe width="560" height="315" src="https://www.youtube.com/embed/fuXSoyuXBYM?si=8hUp6Ox6G5Piddzu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+<h3>random song to cheer you up </h3>
+</div>
    </div>
   )
 }
