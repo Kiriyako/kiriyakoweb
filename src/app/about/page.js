ee588a505470d4fb01dc358b141790fb5fb15ac2
Kiriyako <doraemonboy158@gmail.com>
2023-12-08 22:07:49 +0530

    stuff

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,9 +2,10 @@ import Link from "next/link"
 export default function About() {
   return (
     <div id="main">
-      <h1 style={{ fontSize: '30px' }}>Hi. I am Kiriyako, just a random high-schooler from India. I like making web applications (though I suck). I&apos;m very much interested in Japanese culture and have been learning the language for almost 3 years now. I also own <Link href={`https://project-amai.github.io/about`}>project-amai.</Link></h1>
+      <h1 style={{ fontSize: '30px' }}>Hey there! I'm Kiriyako, a first-year high schooler from India. I like making web apps (though I suck). I LOVE Japanese culture and have been learning the language for almost 3 years now.</h1>
       <br></br>
-      <h1 style={{ fontSize: '30px' }}>やーほ！ Kiriyakoと申します。インドから来ました。高校1年生です。 ウェブアプリを作るが好きです (けど、へたくそです). 日本の文化は面白いだと思いますそして3年くらい日本語を勉強します。</h1>
+      <h1 style={{ fontSize: '30px' }}>やーほ！ Kiriyakoと申します。インドから来ました。高校1年生です。 ウェブアプリを作るが好きです (けど、へたくそです). 日本の文化が好きそして3年くらい日本語を勉強します。</h1>
     </div>
   )
 }
+  
