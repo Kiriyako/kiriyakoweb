50ef4507a867ee5336630cd5210d89d9574a8f96
Kiriyako <doraemonboy158@gmail.com>
2024-01-13 20:40:32 +0530

    stuff

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,9 +2,7 @@ export default function About() {
   return (
     <div id="main">
       <h1 style={{ fontSize: '30px' }}>Hey there! I'm Kiriyako, a first-year high schooler from India. I like making web apps (though I suck). I LOVE Japanese culture and have been learning the language for almost 3 years now.</h1>
-      <br></br>
-      <h1 style={{ fontSize: '30px' }}>やーほ！ Kiriyakoと申します。インドから来ました。高校1年生です。 ウェブアプリを作るが好きです (けど、へたくそです). 日本の文化が好きそして3年くらい日本語を勉強します。</h1>
+      <h1 style={{ fontSize: '30px', marginTop: '20px' }}>やーほ！ Kiriyakoと申します。インドから来ました。高校1年生です。 ウェブアプリを作るが好きです (けど、へたくそです). 日本の文化が好きそして3年くらい日本語を勉強します。</h1>
     </div>
   )
 }
-  
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Main(){
   
    <h1>Hi, I am Kiriyako.</h1>
 <div id="vid">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/DSL4ktjoF_U?si=-06S4bST6OIjS4eA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><h3>random song to cheer you up </h3>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/zggbCjFbRzE?si=f-xkABvotIhWyWJy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe><h3>random song to cheer you up </h3>
 </div>
    </div>
   )
